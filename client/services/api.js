e12d15ded3fd2afb07b01514e755d1205d78be91
raghavendraVIT <raghavendrajob23@gmail.com>
2025-11-07 23:59:43 +0530

    Create api.js

---
+++ client/services/api.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+
+const api = axios.create({
+    baseURL: 'http://localhost:5000/api', 
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+// Request Interceptor: Attach JWT token to every request
+api.interceptors.request.use(
+    (config) => {
+        const user = JSON.parse(localStorage.getItem('user'));
+        const token = user?.token;
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default api;
